23c5600db8913bb411d35a2836bd1aa4e460fe11
Kasirajan <kasimurugasen1995@gmail.com>
2017-08-28 19:13:07 +0530

    Floyds Triangle

---
+++ new.py
@@ -0,0 +1,12 @@
+i=1
+n=int(input("Enter the length of the floyds triangle"))
+count=1
+z=1
+k=0
+while z in range(n+1):
+    for k in range(z):
+        print(i,end="")
+        i+=1
+    k=0
+    z+=1
+    print("\n")
