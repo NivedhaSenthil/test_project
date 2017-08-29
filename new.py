4527acf81dd9374ab3e226c916fae41724f3f195
Kasirajan <kasimurugasen1995@gmail.com>
2017-08-29 14:29:39 +0530

    To check odd or even with suitable conditions
    Task 
    Given an integer, , perform the following conditional actions:
    
    If  is odd, print Weird
    If  is even and in the inclusive range of 2 to5 , print Not Weird
    If  is even and in the inclusive range of  6 to20 , print Weird
    If  is even and greater than 20 , print Not Weird
    Input Format
    
    A single line containing a positive integer, .
    
    Constraints
    
    Output Format
    
    Print Weird if the number is weird; otherwise, print Not Weird.
    
    Sample Input 0
    
    3
    Sample Output 0
    
    Weird
    Sample Input 1
    
    24
    Sample Output 1
    
    Not Weird
    Explanation
    
    Sample Case 0:  
     is odd and odd numbers are weird, so we print Weird.
    
    Sample Case 1:  
     and  is even, so it isn't weird. Thus, we print Not Weird.

--- new.py
+++ new.py
@@ -1,11 +1,5 @@
-i=1
-n=int(input("Enter the length of the floyds triangle"))
-z=1
-k=0
-while z in range(n+1):
-    for k in range(z):
-        print(i,end="")
-        i+=1
-    k=0
-    z+=1
-    print("\n")
+a=int(input())
+if a%2!=0 or a>=6 and a<=20:
+    print("Weird")
+else:
+        print("Not Weird")
